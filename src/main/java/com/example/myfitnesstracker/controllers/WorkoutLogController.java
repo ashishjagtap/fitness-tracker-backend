9800b107d96d1a6d35bba0e8a864cd7cfe7e588b
ashishjagtap <ashish@Ashishs-MacBook-Pro-2.local>
2025-02-18 21:07:39 +0530

    added front end and changed CORS

--- src/main/java/com/example/myfitnesstracker/controllers/WorkoutLogController.java
+++ src/main/java/com/example/myfitnesstracker/controllers/WorkoutLogController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/workouts")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class WorkoutLogController {
 
     @Autowired
